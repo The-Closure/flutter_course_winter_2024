b8e9e6fa2ae7a4846fcf9351dc22dc57794ed992
AbdTll <abdalaziztll992010@gmail.com>
2023-12-10 06:56:22 -0800

    init

---
+++ bin/intro_to_dart.dart
@@ -0,0 +1,42 @@
+void main() {
+//   int counter = 20;
+//   print(counter);
+//   double price = 30.9;
+
+//   String name = "Abdalaziz";
+
+//   bool isTrue = true;
+
+//   print(price);
+
+//   print(name);
+//   print(isTrue);
+
+// var age = '19';
+
+// age ='20';
+
+// dynamic hours =24;
+// print(hours.runtimeType);
+// hours = "25";
+// print(hours.runtimeType);
+
+//  int quantity =0;
+// print(quantity);
+
+
+final String nickName;
+
+nickName = 'Mutaz';
+
+print(nickName);
+// nickName = 'AboAbdo';
+
+const String middleName = 'Tello';
+
+print(middleName);
+
+// middleName = 'Mhd';
+
+
+}

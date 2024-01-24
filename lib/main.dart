9aa9adebf0dab37331fbbe5958c761fb57b0dfe6
AbdTll <abdalaziztll992010@gmail.com>
2024-01-24 06:48:00 -0800

    init point

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+List<String> names = ["Ahmad", "Yaser", "Noor", "Omar"];
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -24,96 +27,127 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey<ScaffoldState> key = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: width / 2,
-          child: TextField(
-            onChanged: (ca) {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondPage(),
-                  ));
-            },
+      key: key,
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        key.currentState!.openDrawer();
+      }),
+      drawerEnableOpenDragGesture: false,
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: UserAccountsDrawerHeader(
+                  currentAccountPicture: CircleAvatar(
+                    child: Icon(Icons.person),
+                  ),
+                  accountName: Text("abdalaziztll992010@gmail.com"),
+                  accountEmail: FlutterLogo()),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        leading: Container(),
+        title: RichText(
+          text: TextSpan(
+            style: TextStyle(fontSize: 40, color: Colors.red),
+            text: 'H',
+            children: [
+              TextSpan(text: 'E'),
+              TextSpan(style: TextStyle(color: Colors.amber), text: 'L'),
+              TextSpan(style: TextStyle(color: Colors.amber), text: 'L'),
+              TextSpan(
+                text: 'O',
+                children: [
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.middle,
+                    child: FlutterLogo(
+                      size: 50,
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          DropdownMenu<String>(
+            label: Text("Please choose one item"),
+            
+              dropdownMenuEntries: List.generate(names.length, (index) {
+            return DropdownMenuEntry(value: names[index], label: names[index]);
+          })),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              setState(() {
+                names[0] = value;
+              });
+            },
+            child: Text(names[0]),
+            initialValue: names[0],
+            itemBuilder: (context) => [
+              PopupMenuItem(child: Text(names[1]), value: names[1]),
+              PopupMenuItem(
+                child: Text(names[2]),
+                value: names[2],
+              ),
+              PopupMenuItem(
+                child: Text(names[3]),
+                value: names[3],
+              )
+            ],
+          ),
+          Center(
+              child: InkWell(
+            onTap: () {
+              ScaffoldMessenger.of(context).showMaterialBanner(
+                  new MaterialBanner(
+                      elevation: 20,
+                      backgroundColor: Colors.red,
+                      content: Text("Title"),
+                      actions: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.abc))
+                  ]));
 
-class SecondPage extends StatelessWidget {
-  SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
-    
-    return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: width / 3,
-              height: height / 4,
-              color: Colors.red,
+              // showAdaptiveDialog(
+              //   barrierDismissible: false,
+              //   context: context,
+              //   builder: (context) => AlertDialog.adaptive(
+              //     title: Text("NOTE !!"),
+              //     content: Text("Are you sure want to Delete ?"),
+              //     actions: [
+              //       CupertinoDialogAction(
+              //         child: Text("Yes"),
+              //       ),
+              //       CupertinoDialogAction(
+              //         child: Text("No"),
+              //       )
+              //     ],
+              //   ),
+              // );
+            },
+            child: Container(
+              child: Tooltip(
+                  richMessage: WidgetSpan(
+                      child: Container(
+                    child: Text('...'),
+                    color: Colors.teal,
+                    width: 30,
+                    height: 30,
+                  )),
+                  child: FlutterLogo()),
             ),
-            SizedBox(
-              child: Container(
-                color: Colors.blue,
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Hello",
-                    style: TextStyle(fontSize: 100),
-                  ),
-                ),
-              ),
-              width: width / 2,
-              height: 100,
-            ),
-            Align(
-              child: FlutterLogo(),
-            ),
-            // AspectRatio(
-            //   aspectRatio: 16 / 9,
-            //   child: Container(
-            //     color: Colors.yellow,
-            //   ),
-            // ),
-            Expanded(
-              flex: 4,
-              child: GridView.builder(
-                itemCount: 10,
-                gridDelegate:
-                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 20),
-                itemBuilder: (context, index) => CircleAvatar(),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Container(
-                color: Colors.teal,
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.green,
-              ),
-            )
-          ],
-        ),
+          )),
+        ],
       ),
     );
   }
